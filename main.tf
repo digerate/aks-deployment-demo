2b158f8226e31cfb0bde6532d4292430f9c3d89f
digerate <craig.wong@digeratesystem.net>
2024-03-25 14:18:13 +1300

    Add the AKS deployment

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "${var.cluster_name}-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2"
+    enable_auto_scaling = true
+    min_count = 1
+    max_count = 2
+  }
+
+  linux_profile {
+    admin_username = "azureuser"
+
+    ssh_key {
+      key_data = var.ssh_public_key
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
