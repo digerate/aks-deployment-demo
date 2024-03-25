b9974e974b276850b4752d5974b6b21d0fb41a1e
digerate <craig.wong@digeratesystem.net>
2024-03-25 13:57:41 +1300

    Delete enhanced-node-app directory

--- enhanced-node-app/src/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String, // Note: In production, ensure passwords are hashed!
-  email: String
-});
-
-module.exports = mongoose.model('User', userSchema);
